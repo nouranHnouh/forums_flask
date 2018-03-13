ae170e9a94372865ddcc03874a6a248f1b36f8ea
Nouran Nouh <nouran.nouh1995@gmail.com>
2018-03-12 21:13:25 -0400

    allow user to delete or edit posts

--- app/views.py
+++ app/views.py
@@ -17,3 +17,30 @@ def topic_add():
 
     else:
         return render_template("topic_add.html")
+
+@app.route("/topic/update/<int:id>",methods=["GET","POST"])
+def topic_update(id):
+	post=post_store.get_by_id(id)
+	if post is None:
+		abort(404)
+	if request.method=="POST":
+		post.title=request.form["title"]
+		post.content=request.form["content"]
+		post_store.update(post)
+		return redirect(url_for("home"))
+	elif request.method=="GET":
+		return render_template("topic_update.html",post=post)
+
+@app.route("/topic/show/<int:id>")
+def topic_show(id):
+	post=post_store.get_by_id(id)
+	if post is None:
+		abort(404; "couldnot find this topic")
+	else:
+		return render_template("topic_show.html",post=post) 
+
+@app.route("/topic/delete/<int:id>")
+def topic_delete(id):
+	post_store.delete(id)
+	return redirect(url_for("home"))
+	
